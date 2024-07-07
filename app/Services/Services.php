61576d29d9b322c682742707130dbd5634b55632
Chin Jing Siang <jingsiang142318@gmail.com>
2024-07-07 21:44:00 +0800

    try

---
+++ app/Services/Services.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Services;
+
+use Twilio\Rest\Client;
+use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+
+class Services
+{
+    public function ImageResizeService($image, $storingFolder)
+    {
+        $imageName = null;
+        if ($image) {
+            $imageName = $image->getClientOriginalName();
+            $destinationPath = public_path($storingFolder);
+            $manager = new ImageManager(new Driver());
+            $img = $manager->read($image);
+            $newImgWidth = $img->width() * 0.75;
+            $img->scaleDown(width:$newImgWidth);
+            //$img->scaleDown(height:500);
+            //images is the location
+            $img->save($destinationPath.'/'.$imageName); 
+        }
+        return $imageName;          
+    }
+
+    public function sendWhatsappMessage(array $recipients, $message){
+        $client = new Client($_ENV['TWILIO_SID'], $_ENV['TWILIO_AUTH_TOKEN']);
+        foreach ($recipients as $recipient) {
+            // dump($_ENV['TWILIO_WHATSAPP_FROM']);
+            // dump($recipient['phone']);
+            $client->messages->create(
+                "whatsapp:{$recipient['phone']}",
+                [
+                    'from' => "whatsapp:".$_ENV['TWILIO_WHATSAPP_FROM'],
+                    'body' => $message,
+                ]
+            );
+        }
+    }
+}
+
+?>
